d29ac117ed515d61fb13b386e3ae065fd49c12d9
Biiba <liesha.rubliev@mail.ru>
2025-05-27 11:57:45 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,204 @@
+def load_bot_configs():
+    crypto_addresses = {
+        "TON": "UQBLNUOpN5B0q_M2xukAB5MsfSCUsdE6BkXHO6ndogQDi5_6",
+        "BTC": "bc1q5xq9m473r8nnkx799ztcrwfqs0555fs3ulw9vr",
+        "USDT": "TQzs3V6QHdXb3CtNPYK9iPWuvvrYCPt6vE"
+    }
+    return {
+        "bot1": {
+            "TOKEN": "7669060547:AAF1zdVIBcmmFKQGhQ7UGUT8foFKW4EBVxs",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002640947060,
+            "PRICE": 600.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot2": {
+            "TOKEN": "8173622705:AAE88BPX5k1mHuwFFBlWJS8ixxa36EmuCC0",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002609563244,
+            "PRICE": 3.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot3": {
+            "TOKEN": "7946129764:AAGaQQXbkBqdYw4ftzR0DwzGaxKrC1xXBqQ",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002635743315,
+            "PRICE": 580.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot4": {
+            "TOKEN": "7737672035:AAHpIGap7ZLt2eE1ZRT6j6YeSdnIuBp2Gqw",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002606081226,
+            "PRICE": 550.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot5": {
+            "TOKEN": "7249884916:AAErSUZlJh552jcmyHVBp1BKJQC6MlC5ldM",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002642788126,
+            "PRICE": 600.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot6": {
+            "TOKEN": "7796522161:AAGLVMbHoMHwqyQKDC6YlVsTMUQBcvb8ZYA",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002357608478,
+            "PRICE": 650.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot7": {
+            "TOKEN": "7986965799:AAFpmrCYKQTFxOanxdvwEKXlsLh84TAoMTU",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002542740564,
+            "PRICE": 600.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot8": {
+            "TOKEN": "8091017735:AAF01_wUHzRCk2Oi3wlDhzRhL8yxKOvx2k8",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002474766276,
+            "PRICE": 600.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot9": {
+            "TOKEN": "7656812252:AAHIALM6ORZr2RgnKolEc-m66IFguRNGuvA",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002692837479,
+            "PRICE": 550.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot10": {
+            "TOKEN": "8195156027:AAFmGf_dltQ9ETpswU3U4UTuWv8eRPS16fU",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002342239719,
+            "PRICE": 650.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot11": {
+            "TOKEN": "7908748621:AAH0XS-abiMUPakjefaVlOommENiCZAcLqA",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002624869413,
+            "PRICE": 660.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot12": {
+            "TOKEN": "7241683107:AAEG6RCRM4Ar1sDYpTV8BsaHfGUj2WXobhI",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002638222463,
+            "PRICE": 500.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot13": {
+            "TOKEN": "7741979722:AAEBzPjM4HqoTdNajwdv2plXvdraARgMbhQ",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002459699589,
+            "PRICE": 550.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+        "bot14": {
+            "TOKEN": "7629991596:AAHkBKWyvz7T2MdaItlQcL90YnOi0Zh11tY",
+            "YOOMONEY_WALLET": "4100118178122985",
+            "NOTIFICATION_SECRET": "CoqQlgE3E5cTzyAKY1LSiLU1",
+            "PRIVATE_CHANNEL_ID": -1002456618280,
+            "PRICE": 525.00,
+            "DESCRIPTION": (
+                "Plan: Standard\n"
+                "Cost: {price} 🇷🇺RUB\n"
+                "Duration: 1 month\n\n"
+                "Access to exclusive 18+ channel"
+            ),
+            "CRYPTO_ADDRESSES": crypto_addresses
+        },
+    }
